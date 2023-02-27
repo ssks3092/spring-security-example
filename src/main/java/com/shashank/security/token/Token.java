6929e52798753ce0c9af05c30c9143143beb2813
Shashank Shekhar Kaushik <ssks3092@gmail.com>
2023-02-28 01:25:43 +0530

    Adding sample project for spring boot security

---
+++ src/main/java/com/shashank/security/token/Token.java
@@ -0,0 +1,42 @@
+package com.shashank.security.token;
+
+import com.shashank.security.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+
+  @Id
+  @GeneratedValue(strategy =  GenerationType.AUTO)
+  public Integer id;
+
+  @Column(unique = true)
+  public String token;
+
+  @Enumerated(EnumType.STRING)
+  public TokenType tokenType = TokenType.BEARER;
+
+  public boolean revoked;
+
+  public boolean expired;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  public User user;
+}
---
+++ src/main/java/com/shashank/security/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.shashank.security.token;
+
+public enum TokenType {
+  BEARER
+}
